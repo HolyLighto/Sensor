e1ccff6be61c0387692cb213e8276b4e6107f23c
HolyLighto <44924682+HolyLighto@users.noreply.github.com>
2022-03-29 19:15:21 +0300

    Добавил комменты

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -8,25 +8,40 @@ namespace TestingConsole
     {
         static void Main(string[] args)
         {
-            List<Sensor> sensorList = Sensor.FillList(3);
+            //количество датчиков
+            int sensorCount = 3;
+            //количество итераций для каждого из датчиков
+            int iterationCount = 10;
+            //Список(массив) датчиков, создаем 3 штуки
+            List<Sensor> sensorList = Sensor.FillList(sensorCount); 
+            //Список показаний датчиков, когда температура была предельной(сохраняется название датчика и температура. которая была)
             List<SensorData> warningTemperature = new List<SensorData>();
-            SensorData[,] allData = new SensorData[10,3];
+            //Двумерный массив, в котором будут храниться все показания. 3 датчика и 10 значений у каждого в данном случае
+            SensorData[,] allData = new SensorData[iterationCount, sensorCount];
 
-            for (int i = 0; i < 10; i++)
+            //Делаем 10 итераций для каждого из 3х датчиков
+            for (int i = 0; i < iterationCount; i++)
             {
                 for (int j = 0; j < sensorList.Count; j++)
                 {
+                    //Меняем текущую температуру
                     sensorList[j].SetRandomCurrentValue(0, 50);
+                    //Если температура больше или меньше предельной, 
                     if (sensorList[j].CurrentTemp < sensorList[j].MinTemp || sensorList[j].CurrentTemp > sensorList[j].MaxTemp)
                     {
+                        //то записываем в массив с критическими температурами
                         warningTemperature.Add(new SensorData(sensorList[j]));
                     }
+                    //и в конце в любом случае заполняем массив со всеми показаниями
                     allData[i, j] = new SensorData(sensorList[j]);
                 }
             }
         }
     }
 
+    /// <summary>
+    /// Класс датчика с его параметрами
+    /// </summary>
     public class Sensor
     {
         public int CurrentTemp { get; set; }
@@ -42,6 +57,11 @@ namespace TestingConsole
             MaxTemp = maxTemp;
         }
 
+        /// <summary>
+        /// Создание списка(массива) датчиков
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public static List<Sensor> FillList(int count)
         {
             Random rnd = new Random();
@@ -53,6 +73,11 @@ namespace TestingConsole
             return list;
         }
 
+        /// <summary>
+        /// Меняем текущее значение датчика на рандомное
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
         public void SetRandomCurrentValue(int minValue, int maxValue)
         {
             Random rnd = new Random();
@@ -60,6 +85,9 @@ namespace TestingConsole
         }
     }
 
+    /// <summary>
+    /// Класс, в котором содержатся данные из датчика, используется для сохранения данных
+    /// </summary>
     public class SensorData
     {
         public string Name;
